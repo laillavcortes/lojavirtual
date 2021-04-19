29a5e1846c99ad677a7b553a5a7d3e3f3e2ce3ac
laillavcortes <laillavcortes@hotmail.com>
2021-04-20 00:10:58 +0100

    projetocriado

---
+++ src/main/java/com/lojavirtual/LojavirtualApplication.java
@@ -0,0 +1,19 @@
+package com.lojavirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojavirtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojavirtualApplication.class, args);
+		
+		
+		
+		System.out.println("Banco Rodando");
+		
+		
+	}
+
+}
